d41771e8affaaba7f1ce588246073f25e1279a13
karthikeya <karthikeya777666@gmail.com>
2025-12-29 21:02:30 -0500

    Add multithreaded search for files in main.py
    Introduced a multithreaded approach to search for files matching user input across specified folders, improving performance. Added timing to measure execution duration. Also created fsfs.py with threading import.

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 from pathlib import Path
+import time
+import threading
+
+def search_func(folders, x):
+    for item in folder.rglob("*"):
+        try:
+            if x.lower() in item.name.lower():
+                print(item)
+        except PermissionError:
+            pass
+
+
 
 folders = [
     Path.home(),                     
     Path("C:/Program Files"),
     Path("C:/Program Files (x86)")
-]
+] # searchs thses folders only 
 
 x = input("Enter what you want to find: ")
 
+threads = []
+
+start = time.perf_counter()
+
 for folder in folders:
-    for i in folder.rglob("*"):
-        try:
-            if x.lower() in i.name.lower():
-                print(i)
-        except PermissionError:
-            pass
+    t = threading.Thread(target=search_func, args=(folder, x))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+end = time.perf_counter()
+
+print(f"Time taken: {end - start:.6f} seconds")
+
 
 
